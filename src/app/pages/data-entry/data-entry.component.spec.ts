ea9b4e7ec24fc1a2fdcebdf83999752474e477d3
Andre <amgowano.apm@gmail.com>
2019-05-30 18:08:10 +0300

    fixed failing test at data-entry component

--- src/app/pages/data-entry/data-entry.component.spec.ts
+++ src/app/pages/data-entry/data-entry.component.spec.ts
@@ -6,9 +6,11 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from 'src/app/core/store/reducers';
 import { ActionTrackerWidgetComponent } from 'src/app/shared/modules/action-tracker-table/containers/action-tracker-widget/action-tracker-widget.component';
 import { FormComponent } from 'src/app/shared/modules/action-tracker-table/components/form/form.component';
+import { TextViewComponent } from 'src/app/shared/modules/action-tracker-table/components/text-view/text-view.component';
 import { WidgetItemLoaderComponent } from 'src/app/shared/modules/action-tracker-table/components/widget-item-loader/widget-item-loader.component';
 import { FormInputItemComponent } from 'src/app/shared/modules/action-tracker-table/components/form-input-item/form-input-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ContextMenuModule } from 'ngx-contextmenu';
 import { TextAreaInputComponent } from 'src/app/shared/modules/action-tracker-table/components/data-item/text-area-input/text-area-input.component';
 
@@ -21,6 +23,7 @@ describe('DataEntryComponent', () => {
       imports: [
         FormsModule,
         ReactiveFormsModule,
+        MatProgressBarModule,
         ContextMenuModule.forRoot(),
         StoreModule.forRoot(reducers, { metaReducers })
       ],
@@ -28,6 +31,7 @@ describe('DataEntryComponent', () => {
         DataEntryComponent,
         ActionTrackerWidgetComponent,
         FormComponent,
+        TextViewComponent,
         WidgetItemLoaderComponent,
         FormInputItemComponent,
         TextAreaInputComponent
