65437386266e36adb4263099d9c60616fb975b35
riquemath <fredabisai@gmail.com>
2020-03-27 08:57:40 +0300

    feat(store): Setup store with reducer and selector for column settings

---
+++ src/app/core/store/selectors/column-settings.selectors.ts
@@ -0,0 +1,64 @@
+import { createSelector } from '@ngrx/store';
+import * as _ from 'lodash';
+
+import { getRootState, State as RootState } from '../reducers';
+import {
+  adapter,
+  ColumnSettingsState
+} from '../reducers/column-settings.reducer';
+
+import { ucFirst } from 'ngx-pipes/src/pipes/helpers/helpers';
+import { getMergedActionTrackerConfiguration } from './action-tracker-configuration.selectors';
+
+export const {
+  selectEntities: getColumnSettingsEntities,
+  selectAll: getColumneSettingsDatas,
+  selectTotal
+} = adapter.getSelectors();
+
+const getColumnSettingsState = createSelector(
+  getRootState,
+  (state: RootState) => state.columnSettings
+);
+export const getColumnSettingsInitialData = createSelector(
+  getMergedActionTrackerConfiguration,
+  actionTrackerConfig => {
+    const { dataElements } = actionTrackerConfig;
+
+    if (dataElements) {
+      return _.map(
+        _.filter(dataElements, element => {
+          return element && !element.isHidden;
+        }),
+        filtered => {
+          if (filtered && filtered.id) {
+            return { id: filtered.id, name: filtered.name, isVisible: true };
+          } else {
+            return '';
+          }
+        }
+      );
+    }
+  }
+);
+export const getColumnSettingsData = createSelector(
+  getColumnSettingsState,
+  getColumnSettingsInitialData,
+  (columnSettingsState: ColumnSettingsState, intialConfig) => {
+    const columnSettings =
+      selectTotal(columnSettingsState) > 0
+        ? getColumneSettingsDatas(columnSettingsState)
+        : intialConfig;
+    const columnSettingObj = {};
+    for (const setting of columnSettings) {
+      const { id, isVisible } = setting;
+      columnSettingObj[id] = isVisible;
+    }
+    return columnSettingObj;
+  }
+);
+
+export const getColumnSettingsDataAfterSet = createSelector(
+  getColumnSettingsState,
+  (state: ColumnSettingsState) => state.loaded
+);
