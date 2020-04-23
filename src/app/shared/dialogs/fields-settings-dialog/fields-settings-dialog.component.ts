bbf29ebb346c1d58d1614646bd3dbdfd6253c9d6
riquemath <fredabisai@gmail.com>
2020-04-23 17:01:34 +0300

    feat: Generate fields-settings-dialog and add it on entry components of shared module

---
+++ src/app/shared/dialogs/fields-settings-dialog/fields-settings-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fields-settings-dialog',
+  templateUrl: './fields-settings-dialog.component.html',
+  styleUrls: ['./fields-settings-dialog.component.css']
+})
+export class FieldsSettingsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
