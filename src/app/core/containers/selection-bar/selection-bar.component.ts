3596dd6186fd38be03fdb6597cb967308cd2cd0d
riquemath <fredabisai@gmail.com>
2020-04-23 16:36:43 +0300

    feat(selection-bar.component): Check authority of login user in selection bar

--- src/app/core/containers/selection-bar/selection-bar.component.ts
+++ src/app/core/containers/selection-bar/selection-bar.component.ts
@@ -3,6 +3,9 @@ import { Observable } from 'rxjs';
 import { SelectionFilterConfig } from 'src/app/shared/modules/selection-filters/models/selected-filter-config.model';
 import { LegendConfigurationDialogComponent } from 'src/app/shared/dialogs/legend-configuration-dialog/legend-configuration-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { getCurrentUserManagementAuthoritiesStatus } from '../../store/selectors/user.selectors';
+import { Store, select} from '@ngrx/store';
+import { State } from '../../store/reducers';
 
 @Component({
   selector: 'app-selection-bar',
@@ -12,11 +15,14 @@ import { MatDialog } from '@angular/material/dialog';
 export class SelectionBarComponent implements OnInit {
   @Input() selectionFilterConfig: SelectionFilterConfig;
   currentPage$: Observable<string>;
+  isAdmin$: Observable<any>;
 
   @Output() filterUpdate: EventEmitter<any> = new EventEmitter<any>();
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private store: Store<State>) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isAdmin$ = this.store.pipe(select(getCurrentUserManagementAuthoritiesStatus));
+  }
 
   onFilterUpdate(selections: any) {
     this.filterUpdate.emit(selections);
