3aa2df6b7496eaf2053d5cf444a9f1b655f46140
riquemath <fredabisai@gmail.com>
2020-04-28 09:49:52 +0300

    feat: get data from table settings selector in table settings dialog checkboxes

--- src/app/shared/dialogs/fields-settings-dialog/fields-settings-dialog.component.ts
+++ src/app/shared/dialogs/fields-settings-dialog/fields-settings-dialog.component.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { State } from 'src/app/core/store/reducers';
 import * as _ from 'lodash';
 import { getDataElementsFromConfiguration } from 'src/app/core/store/selectors/action-tracker-configuration.selectors';
-
+import { getTableFieldsSettings } from 'src/app/core/store/selectors/table-fields-settings.selectors';
+import {
+  CheckMandatorySettingsExistAction,
+  UpdateMandatoryFieldsForTheTableAction,
+} from 'src/app/core/store/actions/table-fields-settings.actions';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-fields-settings-dialog',
   templateUrl: './fields-settings-dialog.component.html',
   styleUrls: ['./fields-settings-dialog.component.css'],
 })
 export class FieldsSettingsDialogComponent implements OnInit {
-  dataElements$: Observable<any>;
-  constructor(private store: Store<State>) {}
+  fields;
+  constructor(
+    private store: Store<State>,
+    public dialogRef: MatDialogRef<FieldsSettingsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   ngOnInit() {
-    this.dataElements$ = this.store.pipe(
-      select(getDataElementsFromConfiguration)
-    );
+    this.fields = [...this.data];
   }
   saveSettings(form, fields) {
-    if(form && form.hasOwnProperty('form')) {
+    if (form && form.hasOwnProperty('form')) {
       const { value } = form.form;
+      const fieldsSettings = _.flattenDeep(
+        _.map(fields, (field) => {
+          if (
+            field &&
+            field.hasOwnProperty('id') &&
+            field.hasOwnProperty('name') &&
+            field.hasOwnProperty('columnMandatory')
+          ) {
+            const { id, name } = field;
+            if (value && value.hasOwnProperty(id) && value[id]) {
+              return { ...{}, id, name, columnMandatory: value[id] } || [];
+            }
+            return { ...{}, id, name, columnMandatory: false } || [];
+          } else {
+            return [];
+          }
+        })
+      );
+
+      this.store.dispatch(
+        new UpdateMandatoryFieldsForTheTableAction({ fieldsSettings })
+      );
+      this.closeDialog('Saved');
     }
-    
+  }
+  closeDialog(action: string) {
+    this.dialogRef.close(action);
   }
 }
