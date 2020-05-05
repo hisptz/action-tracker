3f52c0862bbecb3d257405bc51da3c3b91ff0e6d
riquemath <fredabisai@gmail.com>
2020-05-05 20:31:48 +0300

    feat(action-tracker-data.effects.ts): show snackbar when error occurs

--- src/app/core/store/effects/action-tracker-data.effects.ts
+++ src/app/core/store/effects/action-tracker-data.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { catchError, map, mergeMap, withLatestFrom } from 'rxjs/operators';
 
@@ -20,7 +20,7 @@ import {
   DeleteActionTrackerDataFail,
   DeleteActionTrackerDataSuccess,
   LoadActionTrackerDatas,
-  LoadActionTrackerDatasFail
+  LoadActionTrackerDatasFail,
 } from '../actions/action-tracker-data.actions';
 import { State } from '../reducers';
 import { getRootCauseAnalysisDatas } from '../selectors/root-cause-analysis-data.selectors';
@@ -28,6 +28,7 @@ import { TrackedEntityInstanceService } from '../../services';
 import { getCurrentActionTrackerConfig } from '../selectors/action-tracker-configuration.selectors';
 
 import * as _ from 'lodash';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Injectable()
 export class ActionTrackerDataEffects {
   @Effect({ dispatch: false })
@@ -45,9 +46,18 @@ export class ActionTrackerDataEffects {
                 new AddActionTrackerDatas(_.flatten(actionTrackerDatas))
               );
             }),
-            catchError((error: any) =>
-              of(new LoadActionTrackerDatasFail(error))
-            )
+            catchError((error: any) => {
+              this.store.dispatch(new LoadActionTrackerDatasFail(error));
+              const { message } = error;
+
+              const showMessage = message
+                ? this.showSnackbar(message)
+                : this.showSnackbar(
+                    'Failed to load action tracker data, Please check your internet connection or try again later'
+                  );
+
+              return of(new LoadActionTrackerDatasFail(error));
+            })
           );
       }
     )
@@ -67,7 +77,17 @@ export class ActionTrackerDataEffects {
             (actionTrackerData: any) =>
               new SaveActionTrackerDataSuccess(actionTrackerDataValues)
           ),
-          catchError((error: any) => of(new SaveActionTrackerDataFail(error)))
+          catchError((error: any) => {
+            const { message } = error;
+
+            const showMessage = message
+              ? this.showSnackbar(message)
+              : this.showSnackbar(
+                  'Failed to save action, Please check your internet connection or try again later'
+                );
+
+            return of(new SaveActionTrackerDataFail(error));
+          })
         );
     })
   );
@@ -81,7 +101,17 @@ export class ActionTrackerDataEffects {
           (actionTrackerData: any) =>
             new AddActionTrackerDataSuccess(actionTrackerData)
         ),
-        catchError((error: any) => of(new AddActionTrackerDataFail(error)))
+        catchError((error: any) => {
+          const { message } = error;
+
+          const showMessage = message
+            ? this.showSnackbar(message)
+            : this.showSnackbar(
+                'Failed to add action, Please check your internet connection or try again later'
+              );
+
+          return of(new AddActionTrackerDataFail(error));
+        })
       );
     })
   );
@@ -96,19 +126,34 @@ export class ActionTrackerDataEffects {
           map(
             () => new DeleteActionTrackerDataSuccess(action.actionTrackerDataId)
           ),
-          catchError((error: any) =>
-            of(
+          catchError((error: any) => {
+            const { message } = error;
+
+            const showMessage = message
+              ? this.showSnackbar(message)
+              : this.showSnackbar(
+                  'Failed to delete action, Please check your internet connection or try again later'
+                );
+
+            return of(
               new DeleteActionTrackerDataFail(action.actionTrackerDataId, error)
-            )
-          )
+            );
+          })
         );
     })
   );
+
+  showSnackbar(message: string, action = 'Dismiss', duration = 3000) {
+    this._snackbar.open(message, action, {
+      verticalPosition: 'top',
+    });
+  }
 
   constructor(
     private actions$: Actions,
     private store: Store<State>,
     private actionTrackerDataService: ActionTrackerDataService,
-    private trackedEntityInstanceService: TrackedEntityInstanceService
+    private trackedEntityInstanceService: TrackedEntityInstanceService,
+    private _snackbar: MatSnackBar
   ) {}
 }
