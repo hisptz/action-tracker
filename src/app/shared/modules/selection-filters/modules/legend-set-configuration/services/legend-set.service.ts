c8d439aa621f31c5c5684bb2d70b475df44e3a55
nnkogift <nnkogift@hotmail.com>
2022-04-27 12:05:46 +0300

    fix(legendsets): fixes and improvements
    - Refactored set default legend set function
    - Set default legends whenever legendset array is empty

--- src/app/shared/modules/selection-filters/modules/legend-set-configuration/services/legend-set.service.ts
+++ src/app/shared/modules/selection-filters/modules/legend-set-configuration/services/legend-set.service.ts
@@ -1,44 +1,66 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
 
-import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
-import { LegendSet } from '../models/legend-set';
-import { legendSetMetadata } from 'src/app/core/defaults/action-tracker-legendset-metadata';
+import {NgxDhis2HttpClientService} from '@iapps/ngx-dhis2-http-client';
+import {LegendSet} from '../models/legend-set';
+import {legendSetMetadata} from 'src/app/core/defaults/action-tracker-legendset-metadata';
+import * as _ from 'lodash';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class LegendSetService {
-  constructor(private http: NgxDhis2HttpClientService) {}
+  constructor(private http: NgxDhis2HttpClientService) {
+  }
+
+  legendUrl = `dataStore/actionTrackerLegendSets/configuration`;
+
 
   // @todo update url to data store
   getLegendSets(): Observable<LegendSet[]> {
-    const legendUrl = `dataStore/actionTrackerLegendSets/configuration`;
     return new Observable(observer => {
-      this.http.get(legendUrl).subscribe(
+      this.http.get(this.legendUrl).subscribe(
         (response: any) => {
           const legendSets = response.legendSets;
 
-          console.log(response);
+          if (_.isEmpty(legendSets)) {
+            this.setDefaultLegendSets(observer, true);
+            return;
+          }
           observer.next(legendSets);
           observer.complete();
         },
         () => {
-          const legendSets = legendSetMetadata;
-          this.http.post(legendUrl, { legendSets }).subscribe(
-            () => {
-              observer.next([]);
-              observer.complete();
-            },
-            error => {
-              observer.error(error);
-            }
-          );
+          this.setDefaultLegendSets(observer);
         }
       );
     });
   }
 
+  setDefaultLegendSets(observer, namespaceExists = false) {
+    const legendSets = legendSetMetadata;
+    if (namespaceExists) {
+      this.http.put(this.legendUrl, {legendSets}).subscribe(
+        () => {
+          observer.next(legendSets);
+          observer.complete();
+        },
+        error => {
+          observer.error(error);
+        }
+      );
+      return;
+    }
+    this.http.post(this.legendUrl, {legendSets}).subscribe(
+      () => {
+        observer.next(legendSets);
+        observer.complete();
+      },
+      error => {
+        observer.error(error);
+      }
+    );
+  }
+
   updateLegendSets(legendSets: LegendSet[]): Observable<any> {
-    const legendUrl = `dataStore/actionTrackerLegendSets/configuration`;
-    return this.http.put(legendUrl, { legendSets });
+    return this.http.put(this.legendUrl, {legendSets});
   }
 }
